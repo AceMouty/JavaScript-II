dbce23df11587d28905ff87d4de75681057f9727
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-07-23 18:01:57 -0500

    Reformatted files

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,12 +18,15 @@ twoNums();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+	
 	// Return a function that when invoked increments and returns a counter variable.
 	let count = 0;
+
 	return function(){
 		count += 1
 		return count;
 	}
+	
 };
 // Example usage: const newCounter = counter();
 const newCounter = counter();
