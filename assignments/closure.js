eaa469648690517efbd6d1a9ff3cc8e4c352cb53
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-07-23 18:00:35 -0500

    Completed closure challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,34 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const twoNums = () => {
+	let num1 = 2;
+	let num2 = 5;
 
+	let multiply = () => {
+		return num1 * num2;
+	}
+
+	return console.log(multiply());
+}
+
+twoNums();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+	// Return a function that when invoked increments and returns a counter variable.
+	let count = 0;
+	return function(){
+		count += 1
+		return count;
+	}
 };
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
